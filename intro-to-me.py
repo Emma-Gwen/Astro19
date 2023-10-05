375da13b175e64574fe6b00515999529fd5090bf
Emma-Gwen <146883906+Emma-Gwen@users.noreply.github.com>
2023-10-04 21:35:48 -0700

    Add files via upload

---
+++ intro-to-me.py
@@ -0,0 +1,13 @@
+def IntroToMe():
+	FirstName = "Emma Gwen"
+	LastName = "Churchill"
+	Pronouns = "she/her and he/him"
+	FaveMovie = "Pan's Labyrinth"
+	FaveFood = "sourdough bread"
+	print(f"Hello! My name is {FirstName} {LastName} and my pronouns are {Pronouns}. My favorite movie is {FaveMovie}, which my dad made me watch--it is so delightfully creepy. My favorite food is {FaveFood}, because it slaps toasted, untoasted, with butter or as a sandwich, etc. ")
+
+def main():
+	IntroToMe()
+
+if __name__=="__main__":
+	main()
